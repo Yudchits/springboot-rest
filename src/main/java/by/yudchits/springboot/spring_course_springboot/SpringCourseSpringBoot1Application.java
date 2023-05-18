1548db14936a59636bc9dad466a5a2eb77eae73c
Yudchits <pasha.01.03.2003@mail.ru>
2023-05-18 13:46:10 +0300

    initializing projects and specify properties

---
+++ src/main/java/by/yudchits/springboot/spring_course_springboot/SpringCourseSpringBoot1Application.java
@@ -0,0 +1,11 @@
+package by.yudchits.springboot.spring_course_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseSpringBoot1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCourseSpringBoot1Application.class, args);
+    }
+}
